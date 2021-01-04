183150d1774d4bc34fb6d3d1fb8989f56169a80e
Blue <62485293+kamelzarandah@users.noreply.github.com>
2021-01-04 05:00:47 -0800

    Update server.lua

--- syn_cannon/server.lua
+++ syn_cannon/server.lua
@@ -20,31 +20,19 @@ RegisterCommand("rotatecannon", function(source, args) --- example /rotate 20 (r
   end
 end)
 
-
-
-RegisterCommand("cannon1",function()
-  local _source = source
-    local Character = VorpCore.getUser(_source).getUsedCharacter
-    local rotation =  args[1]
-    local job = "army"
-    local job2 = Character.job
-    local type = 1 
-    if job == job2 then
-      TriggerClientEvent('syn_cannon:spawncannon', type)
-    else return false end
-
-
+RegisterCommand("cannon", function(source, args) --- example /rotate 20 (rotation number from 0 to 360)
+  if args ~= nil then
+    if args[1] == 1 or args[1] == 2 then 
+      local _source = source
+      local Character = VorpCore.getUser(_source).getUsedCharacter
+      local type = args[1]
+      local job = "army"
+      local job2 = Character.job
+      if job == job2 then
+        TriggerClientEvent('syn_cannon:spawncannon', type)
+      else return false end
+    end
+  end
 end)
 
-RegisterCommand("cannon2",function()
-  local _source = source
-    local Character = VorpCore.getUser(_source).getUsedCharacter
-    local rotation =  args[1]
-    local job = "army"
-    local job2 = Character.job
-    local type = 2 
-    if job == job2 then
-      TriggerClientEvent('syn_cannon:spawncannon', type)
-    else return false end
 
-end)
